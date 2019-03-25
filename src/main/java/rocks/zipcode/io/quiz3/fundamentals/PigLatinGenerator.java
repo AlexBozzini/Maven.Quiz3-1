b79db4612a442c6e3170d993f068cb9a6fc34acf
Alexandra Bozzini <alexandrabozzini@zipcoders-MacBook-Pro-74.local>
2019-03-25 09:40:41 -0400

    Initial commit

--- src/main/java/rocks/zipcode/io/quiz3/fundamentals/PigLatinGenerator.java
+++ src/main/java/rocks/zipcode/io/quiz3/fundamentals/PigLatinGenerator.java
@@ -5,6 +5,26 @@ package rocks.zipcode.io.quiz3.fundamentals;
  */
 public class PigLatinGenerator {
     public String translate(String str) {
-        return null;
+        String[] words = str.split(" ");
+        StringBuilder builder = new StringBuilder();
+
+        for(int i = 0; i < words.length; i++){
+            String word = words[i];
+            if (VowelUtils.startsWithVowel(word)){
+                builder.append(word + "way" + " ");
+            } else {
+                Integer vowelIndex = VowelUtils.getIndexOfFirstVowel(word);
+                if (vowelIndex == -1) {
+                    builder.append(word + "ay" + " ");
+                } else {
+                    String beginning = word.substring(0, vowelIndex);
+                    String ending = word.substring(vowelIndex);
+                    String finalWord = ending + beginning + "ay";
+                    builder.append(finalWord + " ");
+                }
+            }
+        }
+        builder.deleteCharAt(builder.length() - 1);
+        return builder.toString();
     }
 }
